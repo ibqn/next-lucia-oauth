4905231231126e8f58552b7f4386cc3ab3e4dcd8
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-02-09 21:02:27 +0100

    remove GitHub OAuth callback and user creation logic from frontend

--- packages/frontend/src/action/user.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import { createUser as createUserDb } from "database/src/queries/user"
-
-export const createUser = async (username: string, email: string, avatarUrl: string) => {
-  return createUserDb(username, email, avatarUrl)
-}
